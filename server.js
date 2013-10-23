98b4308ea12fe0b3789ab0f000b52a1883d9d006
Jonathan Tran <jonathan@perceptualnet.com>
2013-10-23 15:32:47 -0400

    Fixed 404 on join links.

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var renderChatApp = function(req, res) {
 };
 
 app.get('/', renderChatApp);
-app.get('/join', renderChatApp);
+app.get('/join/*', renderChatApp);
 app.get('/login', renderChatApp);
 app.get('/rooms/*', renderChatApp);
 
